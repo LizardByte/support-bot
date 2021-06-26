2e1957d36ccbe5b660bc335586447325f449a1c0
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2021-06-26 00:51:21 +0000

    -reduced number of commands -add arguments to commands -message can now mention a user -donate message improved

--- retroarcher_bot.py
+++ retroarcher_bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand
+from discord_slash.utils.manage_commands import create_option, create_choice
 import os
 import requests
 import urllib
@@ -20,6 +21,11 @@ guild_ids = [int(os.environ['server_id'])]
 botName = 'RetroArcher bot'
 botUrl = 'https://github.com/RetroArcher'
 iconUrl = 'https://raw.githubusercontent.com/RetroArcher/RetroArcher.branding/main/logos/RetroArcher-white-256x256.png'
+
+
+# context reference
+# https://discordpy.readthedocs.io/en/latest/ext/commands/api.html#discord.ext.commands.Context
+
 
 # command : wiki-file
 command_dict = {
@@ -72,7 +78,7 @@ async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="the RetroArcher server"))
 
 '''
-# on message
+# on message, no slash command created
 @client.event
 async def on_message(message):
     print(message)
@@ -98,9 +104,8 @@ async def on_message(message):
             break
 '''
 
-# context reference
-# https://discordpy.readthedocs.io/en/latest/ext/commands/api.html#discord.ext.commands.Context
-
+'''
+# slash command for each wiki page in dictionary
 # Commands
 for key, value in command_dict.items():
     description = value.replace('-', ' ').replace('_', ' ').strip()
@@ -118,15 +123,95 @@ for key, value in command_dict.items():
         embed.set_author(name=botName, url=botUrl, icon_url=iconUrl)
 
         await ctx.send(embed=embed)
+'''
 
 
-@slash.slash(name="donate", description="donate", guild_ids=guild_ids)
-async def _ping(ctx): 
-        embed = discord.Embed(title="Donate", color=0xE5A00D)
-        embed.add_field(name="Github", value="[Link](https://github.com/sponsors/ReenigneArcher)", inline=False)
-        embed.add_field(name="Patreon", value="[Link](https://www.patreon.com/RetroArcher)", inline=False)
-        embed.add_field(name="PayPal", value="[Link](https://paypal.me/ReenigneArcher)", inline=False)
+# Donate command... let's improve this
+@slash.slash(name="donate_old", description="donate", guild_ids=guild_ids)
+async def _ping(ctx):
+    embed = discord.Embed(title="Donate", color=0xE5A00D)
+    embed.add_field(name="Github", value="[Link](https://github.com/sponsors/ReenigneArcher)", inline=False)
+    embed.add_field(name="Patreon", value="[Link](https://www.patreon.com/RetroArcher)", inline=False)
+    embed.add_field(name="PayPal", value="[Link](https://paypal.me/ReenigneArcher)", inline=False)
+    await ctx.send(embed=embed)
+
+
+# Donate command
+@slash.slash(name="donate",
+             description="Support the development of RetroArcher",
+             options=[
+                 create_option(
+                    name='user',
+                    description='Enter the username to mention',
+                    option_type=6,
+                    required=True
+                    )
+                 ],
+             guild_ids=guild_ids
+             )
+async def donate(ctx, user: discord.Member = None):
+    embeds = []
+    embeds.append(discord.Embed(color=0x333))
+    embeds[-1].set_author(name='Github Sponsors', url='https://github.com/sponsors/ReenigneArcher', icon_url='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png')
+
+    embeds.append(discord.Embed(color=0xf96854))
+    embeds[-1].set_author(name='Patreon', url='https://www.patreon.com/RetroArcher', icon_url='https://th.bing.com/th/id/OIP.suF0Aufc2rX2gzgPB2mXpAHaHa?pid=ImgDet&rs=1')
+
+    embeds.append(discord.Embed(color=0x003087))
+    embeds[-1].set_author(name='PayPal', url='https://paypal.me/ReenigneArcher', icon_url='https://img.etimg.com/thumb/msid-60762134,width-300,imgsize-9757,resizemode-4/paypal-reduces-remittance-certificate-charges-by-50-for-small-sellers.jpg')
+
+    if user:
+        await ctx.send('Thank you for your support %s!' % (user.mention), embeds=embeds)
+    else:
+        await ctx.send('Thank you for your support!', embeds=embeds)
+
+
+# Combine all wiki pages into one slash command with options/choices
+# Wiki Command
+wiki_choices = []
+for key, value in command_dict.items():
+    description = value.replace('-', ' ').replace('_', ' ').strip()
+    wiki_choices.append(create_choice(name=key, value=key))
+
+@slash.slash(name="wiki",
+             description="Return any of the listed Wiki pages as a message.",
+             options=[
+                 create_option(
+                    name='page',
+                    description='Return the selected Wiki page as a message',
+                    option_type=3,
+                    required=True,
+                    choices=wiki_choices
+                    ),
+                 create_option(
+                    name='user',
+                    description='Enter the username to mention',
+                    option_type=6,
+                    required=False
+                    )
+                 ],
+             guild_ids=guild_ids
+             )
+
+async def wiki(ctx, page: str, user: discord.Member = None):
+    v = command_dict[page]
+    print(user)
+
+    gitUser = 'RetroArcher'
+    gitRepo = 'RetroArcher.bundle'
+    wikiFile = urllib.parse.quote(v)
+    title = v.replace('-', ' ').replace('_', ' ').strip()
+    color=0xE5A00D
+
+    url, embed_message, color = discordMessage(gitUser, gitRepo, wikiFile, color)
+    embed = discord.Embed(title=title, url=url, description=embed_message, color=color)
+    embed.set_author(name=botName, url=botUrl, icon_url=iconUrl)
+
+    if user:
+        await ctx.send(user.mention, embed=embed)
+    else:
         await ctx.send(embed=embed)
+        
 
 
 # Start the server
