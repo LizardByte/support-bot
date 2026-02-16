5b71cad0057455ab7335df44312a9350806daccc
David Lane <42013603+ReenigneArcher@users.noreply.github.com>
2026-02-16 13:03:57 -0500

    Revert "fix(webapp): bind to localhost instead of 0.0.0.0 (#491)"
    This reverts commit 99d29db5612280bb4f9c3bab0f4db4b917a766ab.

--- src/common/webapp.py
+++ src/common/webapp.py
@@ -363,7 +363,7 @@ def run():
     cert_file, key_file = crypto.initialize_certificate()
 
     app.run(
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8080,
         ssl_context=(cert_file, key_file)
     )
