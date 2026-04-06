50dd6bf1af535208607234b762abfd5aa5537687
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2026-04-05 20:18:28 -0400

    feat: add autoban

---
+++ src/discord_bot/cogs/autoban.py
@@ -0,0 +1,81 @@
+# standard imports
+import logging
+import os
+
+# lib imports
+import discord
+from discord.ext import commands
+
+# Get logger for this module
+logger = logging.getLogger(__name__)
+
+
+class AutoBanCog(discord.Cog):
+    """
+    Discord cog that automatically bans any user who posts in the configured autoban channel.
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        """
+        Listen for messages and ban the author if the message is in the autoban channel.
+
+        Any message sent to the channel defined by ``DISCORD_AUTOBAN_CHANNEL_ID`` will result in
+        the author being banned from the guild. All messages from the user within the past 7 days
+        are also deleted. Bot messages are ignored.
+
+        Parameters
+        ----------
+        message : discord.Message
+            The message object that triggered this event.
+        """
+        if message.author.bot:
+            return
+
+        autoban_channel_id = os.getenv('DISCORD_AUTOBAN_CHANNEL_ID')
+        if not autoban_channel_id:
+            return
+
+        if message.channel.id != int(autoban_channel_id):
+            return
+
+        guild = message.guild
+        if not guild:
+            return
+
+        try:
+            await guild.ban(
+                user=message.author,
+                reason="Automatic ban: posted in restricted channel.",
+                delete_message_seconds=604800,  # Delete messages from the past 7 days
+            )
+            logger.warning(
+                "Auto-banned user %s (%s) for posting in channel %s (%s).",
+                message.author,
+                message.author.id,
+                message.channel.name,
+                message.channel.id,
+            )
+        except discord.Forbidden:
+            logger.error(
+                "Missing permissions to ban user %s (%s) in guild %s (%s).",
+                message.author,
+                message.author.id,
+                guild.name,
+                guild.id,
+            )
+        except discord.HTTPException as e:
+            logger.error(
+                "HTTP error while banning user %s (%s): %s",
+                message.author,
+                message.author.id,
+                e,
+                exc_info=True,
+            )
+
+
+def setup(bot: discord.Bot):
+    bot.add_cog(AutoBanCog(bot=bot))
